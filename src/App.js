76fe022b10d1d0c2ae109ba3c1aac435768058dd
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2022-04-10 18:06:12 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Words from './components/words';
 import './App.css';
 
+const file = './bg-en.csv';
+
+
 function App() {
+
+  useEffect(() => {
+    start();
+  }, []);
+
+  const [text, setText] = useState();
+  const [array, setArray] = useState();
+  
+ 
+ 
+  const start = () => {
+  fetch(file)
+    .then( response => response.text() )
+    .then( responseText => {
+        setText( responseText );
+        csvFileToArray(responseText);
+    });
+
+  };
+  
+    const csvFileToArray = (string) => {
+      const csvHeader = string.slice(0, string.indexOf("\r\n")).split(",");
+      const csvRows = string.slice(string.indexOf("\r\n") + 1).split("\r\n");
+  
+      const array = csvRows.map(i => {
+        const values = i.split(",");
+        const obj = csvHeader.reduce((object, header, index) => {
+          object = values[index];
+          return object;
+        }, {});
+        return obj;
+      });
+
+      setArray(array);
+    };
+
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Quiz
+       <Words words={array}  />
       </header>
     </div>
+    
+
   );
 }
 
-export default App;
+export default App; 
---
+++ src/components/words.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+
+function Words(props) {
+    const [ counter, setCounter ] = useState(0);
+    const [ level, setLevel ] = useState(1);
+ 
+ 
+     const [first, setFirst] = useState();
+     const [last, setLast] = useState();
+     const [answered, setAnswered] = useState(false);
+
+     const setRandomWord = () => {
+        
+        setAnswered(!answered); 
+        if (answered) return;
+        setCounter(counter+1);
+        const random = randomIntFromInterval((level-1)*1000,level*1000);
+        const toSet = props.words[random];
+        console.log(random);
+        const values = toSet.split(";"); 
+        setFirst(values[0]);
+        setLast(values[1]);
+      };
+
+      const randomIntFromInterval = (min, max) => { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+     return (
+       <div>
+         
+         <button onClick={() => setRandomWord()} > 
+          {answered?"Answer":"Next word"}
+        </button>
+        <br /><br />
+        <div> {answered?first:last} </div>
+        
+
+        <br />
+        
+        <hr />
+        <span>Level {level} </span> | <span>Count {counter} </span>
+        <hr />
+         <button onClick={() => level>1 && setLevel(level-1)} > 
+         Level -
+        </button>
+
+        <button onClick={() => level<10 && setLevel(level+1)} > 
+         Level +
+        </button>
+    </div>
+    );
+ }
+
+ export default Words;
