adcbc8ed28b99d4b0716000d6973fed97ddc4ad6
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2022-09-18 13:10:47 +0300

    Styled

--- src/App.js
+++ src/App.js
@@ -2,45 +2,127 @@ import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
 import Words from './components/words';
 import './App.css';
+import Cookies from 'js-cookie'
 
 const file = './bg-en.csv';
 
 
 function App() {
 
+  const [text, setText] = useState();
+  const [arrayFrom, setArrayFrom] = useState();
+  const [arrayTo, setArrayTo] = useState();
+  const [selectedFrom, setSelectedFrom] = useState(Cookies.get('languageFrom'));
+  const [selectedTo, setSelectedTo] = useState(Cookies.get('languageTo'));
+
+
   useEffect(() => {
     start();
-  }, []);
+  }, [selectedFrom, selectedTo]);
 
-  const [text, setText] = useState();
-  const [array, setArray] = useState();
-  
- 
- 
-  const start = () => {
-  fetch(file)
-    .then( response => response.text() )
-    .then( responseText => {
-        setText( responseText );
-        csvFileToArray(responseText);
-    });
+  const handleChangeFrom = event => {
+    console.log(event.target.value);
+    Cookies.set('languageFrom', event.target.value, { expires: 30 });
+    setSelectedFrom(event.target.value);
 
   };
-  
-    const csvFileToArray = (string) => {
-      const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
-      setArray(csvRows);
-    };
+  const handleChangeTo = event => {
+    console.log(event.target.value);
+    Cookies.set('languageTo', event.target.value, { expires: 30 });
+    setSelectedTo(event.target.value);
+
+  };
+
+  const start = () => {
+    
+    if(!selectedFrom || !selectedTo) return;
+    const file1 = `./en-${selectedFrom}.csv`;
+    const file2 = `./en-${selectedTo}.csv`;
 
     
+    fetch(file1)
+      .then(response => response.text())
+      .then(responseText => {
+        csvFileToArrayFrom(responseText);
+      });
+      fetch(file2)
+      .then(response => response.text())
+      .then(responseText => {
+        csvFileToArrayTo(responseText);
+      })
+
+  };
+
+  const csvFileToArrayFrom = (string) => {
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+    setArrayFrom(csvRows);
+  };
+  const csvFileToArrayTo = (string) => {
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+    setArrayTo(csvRows);
+  };
+  const css = `
+  .languageSelect {
+      margin:10px;
+      padding:10px;
+  }
+`
   return (
     <div className="App">
-      <header className="App-header">
-        Quiz
-       <Words words={array}  />
+      <style>{css}</style>
+     
+      <header className="App-header" className="fh5co-footer-links">
+      
+      <div style={{display: selectedFrom && selectedTo ? 'none' : 'block' }}>
+        <select name="fromLanguage" id="fromLanguage" value={selectedFrom} onChange={handleChangeFrom} className="languageSelect">
+        <option value="">Language you speak</option>
+          <option value="en">English</option>
+          <option value="ar">Arabic</option>
+          <option value="bn">Bengali</option>
+          <option value="bg">Bulgarian</option>
+          <option value="zh-CN">Chinese</option>
+          <option value="cs">Czech</option>
+          <option value="da">Danish</option>
+          <option value="nl">Dutch</option>
+          <option value="fr">French</option>
+          <option value="de">German</option>
+          <option value="hi">Hindi</option>
+          <option value="id">Indonesian</option>
+          <option value="it">Italian</option>
+          <option value="ko">Korean</option>
+          <option value="pt">Portuguese</option>
+          <option value="ru">Russian</option>
+          <option value="es">Spanish</option>
+        </select><br />
+        <select name="toLanguage" id="toLanguage"  value={selectedTo} onChange={handleChangeTo} className="languageSelect">
+         <option value="">Language you want to learn</option>
+          <option value="en">English</option>
+          <option value="ar">Arabic</option>
+          <option value="bn">Bengali</option>
+          <option value="bg">Bulgarian</option>
+          <option value="zh-CN">Chinese</option>
+          <option value="cs">Czech</option>
+          <option value="da">Danish</option>
+          <option value="nl">Dutch</option>
+          <option value="fr">French</option>
+          <option value="de">German</option>
+          <option value="hi">Hindi</option>
+          <option value="id">Indonesian</option>
+          <option value="it">Italian</option>
+          <option value="ko">Korean</option>
+          <option value="pt">Portuguese</option>
+          <option value="ru">Russian</option>
+          <option value="es">Spanish</option>
+        </select>
+        </div>
+        <Words arrayFrom={arrayFrom} arrayTo={arrayTo} languageFrom={selectedFrom} languageTo={selectedTo} isLoaded={selectedFrom && selectedTo} />
+        <hr />
+        <button  onClick={() => { Cookies.set('languageFrom',''); Cookies.set('languageTo','');window.location.reload(); }} > 
+          Clear
+        </button>
       </header>
     </div>
-    
+
 
   );
 }
--- src/components/words.jsx
+++ src/components/words.jsx
@@ -3,8 +3,7 @@ import React, { useState, useEffect } from 'react';
 function Words(props) {
     const [ counter, setCounter ] = useState(0);
     const [ level, setLevel ] = useState(1);
- 
- 
+    
      const [first, setFirst] = useState();
      const [last, setLast] = useState();
      const [answered, setAnswered] = useState(false);
@@ -15,37 +14,55 @@ function Words(props) {
         if (answered) return;
         setCounter(counter+1);
         const random = randomIntFromInterval((level-1)*1000,level*1000);
-        const toSet = props.words[random];
-        const values = toSet.split(","); 
-        setFirst(values[0]);
-        setLast(values[1]);
+        if(props.languageFrom == 'en'){
+          const toSet = props.arrayTo[random];
+          const values = toSet.split(";"); 
+          setFirst(values[0]);
+          setLast(values[1]);
+        }
+        else if (props.languageTo=='en'){
+          const toSet = props.arrayFrom[random];
+          const values = toSet.split(";"); 
+          setFirst(values[1]);
+          setLast(values[0]);
+        }
+        else{
+          const toSetTo = props.arrayTo[random];
+          const toSetFrom = props.arrayFrom[random];
+          const valuesTo = toSetTo.split(";"); 
+          const valuesFrom = toSetFrom.split(";"); 
+          setFirst(valuesFrom[1]);
+          setLast(valuesTo[1]);
+        }
+       
       };
 
       const randomIntFromInterval = (min, max) => { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min)
       }
+      const css = `
+      .word {
+         font-family: "Helvetica Neue", Times New Roman,Arial, sans-serif; 
+        font-weight: 400;
+        font-size: 22px;
+        line-height: 1.7;
+      }
+      `
      return (
        <div>
-         
-         <button onClick={() => setRandomWord()} > 
-          {answered?"Answer":"Next word"}
+         <style>{css}</style>
+  
+       
+        <hr />
+        <div className='word'> {answered?first:last}&nbsp;</div>
+        <hr />
+        <br />
+        <button  onClick={() => setRandomWord()} style={{visibility: props.isLoaded ? 'visible' : 'hidden' }} > 
+          {answered?"Answer":(counter==0?"Start":"Word "+(counter+1))}
         </button>
-        <br /><br />
-        <div> {answered?first:last} </div>
-        
-
+       
         <br />
         
-        <hr />
-        <span>Level {level} </span> | <span>Count {counter} </span>
-        <hr />
-         <button onClick={() => level>1 && setLevel(level-1)} > 
-         Level -
-        </button>
-
-        <button onClick={() => level<10 && setLevel(level+1)} > 
-         Level +
-        </button>
     </div>
     );
  }
